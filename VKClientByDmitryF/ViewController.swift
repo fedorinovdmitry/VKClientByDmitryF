540be056e30d030dcda968a5ec47bc8d79691fae
skinnerrula <skinklass@mail.ru>
2020-06-09 19:10:02 +0300

    Initial Commit

---
+++ VKClientByDmitryF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKClientByDmitryF
+//
+//  Created by Дмитрий Федоринов on 09.06.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
