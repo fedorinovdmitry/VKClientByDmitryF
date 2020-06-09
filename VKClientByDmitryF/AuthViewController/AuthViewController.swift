e4d1e80638d777258a95da1eee959afba727504b
skinnerrula <skinklass@mail.ru>
2020-06-09 19:44:57 +0300

    add authService and AuthViewController

---
+++ VKClientByDmitryF/AuthViewController/AuthViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AuthViewController.swift
+//  VKClientByDmitryF
+//
+//  Created by Дмитрий Федоринов on 09.06.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: view.center.x - 80,
+                                            y: view.frame.height - ((view.frame.height - imageView.frame.height) / 2 + 30),
+                                            width: 160, height: 60))
+        button.titleLabel?.font = UIFont(name: "Papyrus", size: 30)
+        button.setTitle("Login to VK", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.layer.shadowOpacity = 1
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 1, height: 1)
+        button.layer.shadowRadius = 2
+        button.layer.masksToBounds = false
+        
+        button.addTarget(self, action: #selector(logIn), for: .touchUpInside)
+        
+        
+        return button
+    }()
+    
+    private lazy var authService: AuthService = SceneDelegate.shared().authService
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(loginButton)
+    }
+    
+    @objc private func logIn() {
+        authService.wakeUpSession()
+    }
+}
+
---
+++ VKClientByDmitryF/Services/AuthService.swift
@@ -0,0 +1,74 @@
+//
+//  AuthService.swift
+//  VKClientByDmitryF
+//
+//  Created by Дмитрий Федоринов on 09.06.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import Foundation
+import VKSdkFramework
+
+protocol AuthServiceDelegate: class {
+    func authServiceShouldShow(viewController: UIViewController)
+    func authServiceSignIn()
+    func authServiceSignInDidFail()
+}
+
+final class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    private let appVkId = "7505641"
+    private let vkSdk: VKSdk
+    
+    weak var delegate: AuthServiceDelegate?
+    
+    var token: String? {
+        return VKSdk.accessToken()?.accessToken
+    }
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appVkId)
+        super.init()
+        print("VKSdk.initialize")
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    func wakeUpSession() {
+        let scope = ["offline"]
+        VKSdk.wakeUpSession(scope) { [delegate] (state, error) in
+            switch state {
+            case .initialized:
+                print("initialized")
+                VKSdk.authorize(scope)
+            case .authorized:
+                delegate?.authServiceSignIn()
+                print("authorized")
+            default:
+                delegate?.authServiceSignInDidFail()
+            }
+        }
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+        guard let token = result.token else { return }
+        print(token)
+        delegate?.authServiceSignIn()
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+        delegate?.authServiceSignInDidFail()
+    }
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+        delegate?.authServiceShouldShow(viewController: controller)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+}
+
